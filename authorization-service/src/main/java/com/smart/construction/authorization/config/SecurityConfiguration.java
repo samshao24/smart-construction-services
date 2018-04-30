8acde892f29bd3734e8d7fe787b0fb8230676e03
Samuel Shao <samuel.shao@qualifacts.com>
2018-04-30 10:16:48 -0500

    Make Authorization server to be its own resource server as well for user management purpose.

--- authorization-service/src/main/java/com/smart/construction/authorization/config/SecurityConfiguration.java
+++ authorization-service/src/main/java/com/smart/construction/authorization/config/SecurityConfiguration.java
@@ -1,17 +1,27 @@
 package com.smart.construction.authorization.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    public TokenStore tokenStore;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/ping").permitAll()
